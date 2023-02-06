db9218ca23140f7a79a81e963915143e3ff7ceef
Luca <luca@weve-got-it.com>
2023-02-06 20:01:37 +0200

    Update To Files
    Fixed a spelling error in the css file.
    
    Changed a letter in the html file.
    
    Added some basic JS to the file to toggle the toggle button.

---
+++ 19-50-Theme-Clock/script.js
@@ -0,0 +1,21 @@
+const hourElement = document.querySelector('.hour')
+const minuteElement = document.querySelector('.minute')
+const secondElement = document.querySelector('.second')
+const timeElement = document.querySelector('.time')
+const dateElement = document.querySelector('.date')
+const toggleElement = document.querySelector('.toggle')
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+toggleElement.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark Mode'
+    } else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light Mode'
+    }
+})
